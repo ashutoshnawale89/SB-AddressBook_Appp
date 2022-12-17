80252e08477181b14cdffd563272841b96be575a
ashutoshnawale89 <ashutoshnawale89@gmail.com>
2022-12-17 12:30:40 +0530

    Create: Employee Pay Roll Appp

--- src/main/java/com/bridgelabz/EmployeePayrollApp/EmployeePayrollAppApplication.java
+++ src/main/java/com/bridgelabz/EmployeePayrollApp/EmployeePayrollAppApplication.java
@@ -8,6 +8,7 @@ public class EmployeePayrollAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeePayrollAppApplication.class, args);
+		System.out.println("Welcome To The Employee PayRoll App Project");
 	}
 
 }
