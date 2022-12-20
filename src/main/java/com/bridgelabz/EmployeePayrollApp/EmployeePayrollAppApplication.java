8c54e683f641af8982a8a344a0b8159dc592d9f2
ashutoshnawale89 <ashutoshnawale89@gmail.com>
2022-12-20 09:10:51 +0530

    Added:Determine the logginf levels logging to console or file, logging pattern,etc based o this application running in Dev, Staging or Production

--- src/main/java/com/bridgelabz/EmployeePayrollApp/EmployeePayrollAppApplication.java
+++ src/main/java/com/bridgelabz/EmployeePayrollApp/EmployeePayrollAppApplication.java
@@ -15,6 +15,7 @@ public class EmployeePayrollAppApplication {
 	public static void main(String[] args) {
 		ApplicationContext context=
 				SpringApplication.run(EmployeePayrollAppApplication.class, args);
+
 		System.out.println("Welcome To The Employee PayRoll App Project");
 		log.info("Employee Payroll App Started in {} Environment", context.getEnvironment().getProperty("environment"));
 	}
