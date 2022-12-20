960a1bc8044da2b0b92cd2d173a624b0994a5a67
ashutoshnawale89 <ashutoshnawale89@gmail.com>
2022-12-20 09:24:21 +0530

    Added:Validation Need To Be Done for Both Create and Update Rest Calls

--- src/main/java/com/bridgelabz/EmployeePayrollApp/dto/EmployeePayrollDTO.java
+++ src/main/java/com/bridgelabz/EmployeePayrollApp/dto/EmployeePayrollDTO.java
@@ -4,8 +4,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-    public class EmployeePayrollDTO {
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+public class EmployeePayrollDTO {
+        @NotEmpty(message = "Employee Name Cannot Be Null")
         public String name;
+        @Min(value = 500,message = "Min Wage Should Be More Than 500")
         public long salary;
 
         public EmployeePayrollDTO(String name, long salary) {
